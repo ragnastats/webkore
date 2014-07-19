8d7cee794b7b868af73e493b7afffbdb44baf7d3
Rachel Fish <itsrachelfish@gmail.com>
2014-07-18 21:37:51 -0600

    Basic socket server

--- server/webkore-server.js
+++ server/webkore-server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     io      = require('socket.io').listen(server);
 
 server.listen(1337);
-console.log("Server running!");
+console.log("Web server running on port 1337");
 
 // Create routes for static content and the index
 app.use(express.static(__dirname + '/static'));
@@ -14,3 +14,25 @@ app.get('/', function (req, res)
     console.log("Page requested.");
     res.sendfile(__dirname + '/index.html');
 });
+
+
+var net = require('net');
+
+var HOST = '0.0.0.0';
+var PORT = 1338;
+
+// Create a server instance
+net.createServer(function(socket) {
+    console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
+    
+    socket.on('data', function(data) {
+        console.log('From ' + socket.remoteAddress + ': ' + data);
+        socket.write(data);
+    });
+    
+    socket.on('close', function(data) {
+        console.log('Socket connection closed... ');
+    });
+}).listen(PORT, HOST);
+
+console.log("Statistics server running on " + PORT);
